6361c0908d7250517ba96becbb5faf394b21e803
TH_97 <116719843+TH-97@users.noreply.github.com>
2025-02-10 19:13:00 +0900

    Applying Global Style Using Style Reset

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./routes/home"
 import Profile from "./routes/profile"
 import Login from "./routes/login"
 import CreateAccount from "./routes/create-account"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
 
 const router = createBrowserRouter([
@@ -28,10 +30,25 @@ const router = createBrowserRouter([
     path:"/creat-account",
     element:<CreateAccount/>
   }
-])
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+*{
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+  color : white;
+  font-family : system-ui, -apple-system,BlinkMacSystemFont,'Segoe UI',
+  Roboto,Oxygen, Ubuntu, Cantarell, 'Open Sans','Helvtice Neue',
+  sans-serif;
+}
+`;
 
 function App() {
   return <>
+    <GlobalStyles/>
     <RouterProvider router={router}/>
   </>
 }
