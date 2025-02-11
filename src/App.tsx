76ca9d45fd30c35f5de52f293719b429d1ab5393
TH_97 <hood3392@gmail.com>
2025-02-11 15:33:33 +0900

    Forms and UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { createGlobalStyle } from "styled-components"
 import reset from "styled-reset"
 import { useEffect, useState } from "react"
 import LoadingScreen from "./components/loading-screen"
+import { auth } from "./firebase"
+import styled from "styled-components"
 
 
 const router = createBrowserRouter([
@@ -48,19 +50,27 @@ body {
 }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async() =>{
+    await auth.authStateReady();
     setLoading(false);
   };
   useEffect(()=>{
     init();
 
   },[]);
-  return <>
+  return <Wrapper>
     <GlobalStyles/>
     {isLoading ? <LoadingScreen/> : <RouterProvider router={router}/>}
-  </>
+  </Wrapper>
 }
 
 export default App
